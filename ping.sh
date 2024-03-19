4508e63a00d68d16967a6804a931267eb117b639
wiggy <clange@plasmarack.com>
2024-03-18 19:44:59 -0500

    Update ping.sh

--- ping.sh
+++ ping.sh
@@ -1,39 +1,57 @@
 #!/bin/bash
 
-# Define the list of addresses
-addresses=(
-"de.zephyr.herominers.com"
-"fi.zephyr.herominers.com"
-"ca.zephyr.herominers.com"
-"ru.zephyr.herominers.com"
-"us.zephyr.herominers.com"
-"us2.zephyr.herominers.com"
-"br.zephyr.herominers.com"
-"hk.zephyr.herominers.com"
-"kr.zephyr.herominers.com"
-"in.zephyr.herominers.com"
-"sg.zephyr.herominers.com"
-"tr.zephyr.herominers.com"
-"au.zephyr.herominers.com"
+# List of server addresses
+servers=(
+    "de.zephyr.herominers.com"
+    "fi.zephyr.herominers.com"
+    "ca.zephyr.herominers.com"
+    "ru.zephyr.herominers.com"
+    "us.zephyr.herominers.com"
+    "us2.zephyr.herominers.com"
+    "br.zephyr.herominers.com"
+    "hk.zephyr.herominers.com"
+    "kr.zephyr.herominers.com"
+    "in.zephyr.herominers.com"
+    "sg.zephyr.herominers.com"
+    "tr.zephyr.herominers.com"
+    "au.zephyr.herominers.com"
 )
+
+port=1123
+
+# Variables to keep track of the server with the lowest latency
 minLatency=1000000
-minAddress=""
+minServer=""
 
-for address in "${addresses[@]}"
-do
-    result=$(hping3 --quiet -c 8 -p 1123 -S $address 2>&1 | grep round-trip | awk -F '/' '{print $5}')
+for server in "${servers[@]}"; do
+    echo "Pinging $server..."
 
-    result=${result%.*}
+    # Run paping, extract average latency
+    result=$(paping "$server" -c 3 -p $port | grep 'Average' || echo "Error pinging $server")
 
-    if [[ ! -z "$result" && "$result" =~ ^[0-9]+$ ]]; then
-        echo "$address: $result ms"
-        if [ "$result" -lt "$minLatency" ]; then
-            minLatency="$result"
-            minAddress="$address"
-        fi
-    else
-        echo "No response from $address"
+    # Check if paping encountered an error
+    if [[ $result == *"Error pinging"* ]]; then
+        echo $result
+        continue
+    fi
+
+    # Extract the average latency, removing ANSI color codes and any illegal characters
+    latency=$(echo $result | sed 's/\x1b\[[0-9;]*m//g' | awk -F' ' '{ for(i=1;i<=NF;i++) if ($i=="Average") print $(i+2) }' | sed 's/ms//' | tr -d '[:cntrl:]')
+
+    if [[ -z $latency ]]; then
+        echo "Failed to extract latency for $server."
+        continue
+    fi
+
+    echo "Average latency for $server: $latency ms"
+
+    # Use bc for comparison, ensuring input is properly formatted
+    comparison=$(echo "$latency < $minLatency" | bc 2>/dev/null)
+
+    if [[ $comparison -eq 1 ]]; then
+        minLatency=$latency
+        minServer=$server
     fi
 done
 
-echo "Lowest latency is $minLatency ms at $minAddress"
+echo "The server with the lowest latency is $minServer with an average latency of $minLatency ms."
